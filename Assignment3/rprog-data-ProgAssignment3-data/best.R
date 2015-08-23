95e010ea7d49267ac3a51547756f8497c91705b2
escray <zescray@gmail.com>
2015-08-23 22:35:51 +0800

    Assignment3

---
+++ Assignment3/rprog-data-ProgAssignment3-data/best.R
@@ -0,0 +1,36 @@
+best <- function(state, outcome) {
+        ## Read outcome data
+        data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+        ## Check that state and outcome are valid
+        states <- levels(data$State)
+        outcomes <- c("heart attack", "heart failure", "pneumonia")
+        if( length(grep(state, data$State)) <= 0 ) {
+                stop("invalid state")
+        }
+        
+        
+        column <- 0
+        if (outcome == "heart attack" ){
+                column <- 11
+        }
+        else if (outcome == "heart failure") {
+                column <- 17
+        }
+        else if (outcome == "pneumonia") {
+                column <- 23
+        }
+        else {
+                stop("invalid outcome")
+        }
+                        
+        df <- data[, c(2, 7, column)]
+        names(df) <- c("hospital", "state", "value")
+        df$value <- as.numeric(df$value)
+        df <- df[complete.cases(df), ]
+        
+        orderdata <- df[df$state == state, ]
+        orderdata <- orderdata[order(orderdata[, 3], orderdata[, 2]),]
+        ## return(head(orderdata, 6))
+        head(orderdata)
+        ## Return hospital name in the state with lowest 30-day death rate
+}
